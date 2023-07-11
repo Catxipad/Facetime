a01d0a136e9f2799ed31ead03a14d59b1c08391b
Cristina Lara <99909525+Catxipad@users.noreply.github.com>
2023-07-11 10:16:49 -0400

    Add files via upload

---
+++ facetime.py
@@ -0,0 +1,26 @@
+
+import time
+from pynput.keyboard import Controller
+import subprocess
+
+def make_call(phone_number):
+    subprocess.run(['open', f'facetime://{phone_number}'])
+
+def press_key(key):
+    keyboard = Controller()
+    keyboard.press(key)
+    keyboard.release(key)
+
+def dial_and_press(phone_number):
+    make_call(phone_number)
+    time.sleep(5)  # Adjust delay as needed to allow time for call setup
+    #press_key('9')
+    #time.sleep(1)  # Adjust delay as needed to allow time for the keypress to register
+
+# Example usage
+phone_number = '3478601814'  # Replace with the desired recipient's phone number
+while True:
+    dial_and_press(phone_number)
+    time.sleep(30)
+    print("Call made")
+    # Add any additional logic or conditions for continuing the loop
